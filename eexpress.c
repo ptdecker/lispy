adf0796e6f09d228aa1f1c7789a34fd571a0b874
ptdecker <ptdecker@mac.com>
2014-06-18 22:59:44 -0500

    Added ability to handle built in q-expression functions 'list', 'head', 'tail', 'join', and 'eval'

--- eexpress.c
+++ eexpress.c
@@ -13,6 +13,10 @@
  * Due to the inclusion of GNU Readline, this project is also licensed under GPL v3
  */
 
+/* Define macros */
+
+#define LASSERT(args, cond, err) if (!(cond)) { lval_del(args); return lval_err(err); }
+
 /* Requires sudo apt-get install libedit-dev */
 
 #include <stdio.h>
@@ -86,7 +90,9 @@ void lval_print(lval* v);
 lval* lval_eval(lval* v);
 lval* lval_pop (lval* v, int i);
 lval* lval_take (lval* v, int i);
+lval* builtin(lval* a, char* func);
 lval* builtin_op(lval* a, char* op);
+lval* lval_join(lval* x, lval* y);
 
 /* Construct a pointer to a new number type lisp value */
 
@@ -306,7 +312,7 @@ lval* lval_eval_sexpr(lval* v) {
 
 	/* Call built-in operations evaluator with operator */
 
-	lval* result = builtin_op(v, f->sym);
+	lval* result = builtin(v, f->sym);
 	lval_del(f);
 	return result;
 
@@ -412,6 +418,96 @@ lval* builtin_op(lval* a, char* op) {
 
 }
 
+/* Handle built-in 'head' function */
+
+lval* builtin_head(lval* a) {
+
+  LASSERT(a, (a->count == 1                 ), "Function 'head' passed too many arguments!");
+  LASSERT(a, (a->cell[0]->type == LVAL_QEXPR), "Function 'head' passed incorrect type!");
+  LASSERT(a, (a->cell[0]->count != 0        ), "Function 'head' passed {}!");
+
+  lval* v = lval_take(a, 0);  
+  while (v->count > 1) {
+  	lval_del(lval_pop(v, 1));
+  }
+  return v;
+}
+
+/* Handle built-in 'tail' function */
+
+lval* builtin_tail(lval* a) {
+
+	LASSERT(a, (a->count == 1                 ), "Function 'tail' passed too many arguments!");
+  	LASSERT(a, (a->cell[0]->type == LVAL_QEXPR), "Function 'tail' passed incorrect type!");
+  	LASSERT(a, (a->cell[0]->count != 0        ), "Function 'tail' passed {}!");
+
+  	lval* v = lval_take(a, 0);  
+  	lval_del(lval_pop(v, 0));
+  	return v;
+}
+
+/* Handle built-in 'list' function */
+
+lval* builtin_list(lval* a) {
+	a->type = LVAL_QEXPR;
+	return a;
+}
+
+/* Handle built-in 'eval' function */
+
+lval* builtin_eval(lval* a) {
+
+	LASSERT(a, (a->count == 1                 ), "Function 'eval' passed too many arguments!");
+  	LASSERT(a, (a->cell[0]->type == LVAL_QEXPR), "Function 'eval' passed incorrect type!");
+
+  	lval* x = lval_take(a, 0);
+  	x->type = LVAL_SEXPR;
+  	return lval_eval(x);
+}
+
+/* Handle built-in 'join' function */
+
+lval* builtin_join(lval* a) {
+
+	for (int i = 0; i < a->count; i++) {
+    	LASSERT(a, (a->cell[i]->type == LVAL_QEXPR), "Function 'join' passed incorrect type.");
+  	}
+
+  	lval* x = lval_pop(a, 0);
+
+  	while (a->count) {
+    	x = lval_join(x, lval_pop(a, 0));
+  	}
+
+  	lval_del(a);
+  	return x;
+}
+
+lval* lval_join(lval* x, lval* y) {
+
+  	/* For each cell in 'y' add it to 'x' */
+
+  	while (y->count) {
+    	x = lval_add(x, lval_pop(y, 0));
+  	}
+
+  	/* Delete the empty 'y' and return 'x' */
+
+  	lval_del(y);  
+  	return x;
+}
+
+lval* builtin(lval* a, char* func) {
+  	if (strcmp("list", func) == 0) { return builtin_list(a); }
+  	if (strcmp("head", func) == 0) { return builtin_head(a); }
+  	if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+  	if (strcmp("join", func) == 0) { return builtin_join(a); }
+  	if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+  	if (strstr("+-/*", func)) { return builtin_op(a, func); }
+  	lval_del(a);
+  	return lval_err("Unknown Function!");
+}
+
 /* Start REPL */
 
 int main (int argc, char** argv) {
@@ -428,7 +524,7 @@ int main (int argc, char** argv) {
 	mpca_lang(MPCA_LANG_DEFAULT,
   		" \
     		number : /-?[0-9]+/ ; \
-    		symbol : '+' | '-' | '*' | '/' | '%' ; \
+    		symbol : \"list\" | \"head\" | \"tail\" | \"join\" | \"eval\" |'+' | '-' | '*' | '/' | '%' ; \
     		sexpr  : '(' <expr>* ')' ; \
     		qexpr  : '{' <expr>* '}' ; \
     		expr   : <number> | <symbol> | <sexpr> | <qexpr> ; \
@@ -449,8 +545,10 @@ int main (int argc, char** argv) {
 		add_history(input);
 
 		mpc_result_t r; if (mpc_parse("<stdin>", input, Lispy, &r)) {
-		lval* x = lval_eval(lval_read(r.output));     lval_println(x);
-		lval_del(x); }
+			lval* x = lval_eval(lval_read(r.output));
+			lval_println(x);
+			lval_del(x);
+		}
 
 		free(input);
 	}
