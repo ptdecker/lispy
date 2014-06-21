634c1127fe6f8e2c32c6b099ff79150bf9db225b
ptdecker <ptdecker@mac.com>
2014-06-20 22:17:23 -0500

    Added the scaffold for the 'len' funtion

--- eexpress.c
+++ eexpress.c
@@ -515,6 +515,24 @@ lval* builtin_cons(lval* a) {
   	return x;
 }
 
+/* Handle built-in 'len' function */
+
+lval* builtin_len(lval* a) {
+
+	for (int i = 0; i < a->count; i++) {
+    	LASSERT(a, (a->cell[i]->type == LVAL_QEXPR), "Function 'len' passed incorrect type.");
+  	}
+
+  	lval* x = lval_pop(a, 0);
+
+  	while (a->count) {
+    	x = lval_join(x, lval_pop(a, 0));
+  	}
+
+  	lval_del(a);
+  	return x;
+}
+
 
 lval* builtin(lval* a, char* func) {
   	if (strcmp("list", func) == 0) { return builtin_list(a); }
@@ -523,6 +541,7 @@ lval* builtin(lval* a, char* func) {
   	if (strcmp("join", func) == 0) { return builtin_join(a); }
   	if (strcmp("eval", func) == 0) { return builtin_eval(a); }
   	if (strcmp("cons", func) == 0) { return builtin_cons(a); }
+  	if (strcmp("len", func) == 0) { return builtin_len(a); }
   	if (strstr("+-/*", func)) { return builtin_op(a, func); }
   	lval_del(a);
   	return lval_err("Unknown Function!");
@@ -544,7 +563,7 @@ int main (int argc, char** argv) {
 	mpca_lang(MPCA_LANG_DEFAULT,
   		" \
     		number : /-?[0-9]+/ ; \
-    		symbol : \"list\" | \"head\" | \"tail\" | \"join\" | \"eval\" |'+' | '-' | '*' | '/' | '%' ; \
+    		symbol : \"len\" | \"cons\" | \"list\" | \"head\" | \"tail\" | \"join\" | \"eval\" |'+' | '-' | '*' | '/' | '%' ; \
     		sexpr  : '(' <expr>* ')' ; \
     		qexpr  : '{' <expr>* '}' ; \
     		expr   : <number> | <symbol> | <sexpr> | <qexpr> ; \
